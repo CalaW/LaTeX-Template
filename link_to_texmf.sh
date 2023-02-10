553dedc4a1f8395ce93887c16db9a879a3075132
CalaW <maker_cc@foxmail.com>
2023-02-11 01:25:23 +0800

    new cv template

---
+++ link_to_texmf.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# This script is used to link classes in this directory to the user's texmf directory
+
+ln -s ~/Documents/Program/LaTeX-Template/CalaWCV.cls ~/Library/texmf/tex/latex/calawcls/CalaWCV.cls
