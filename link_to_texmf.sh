61d28c19368ca368ee1df50c5448fc1396c660d0
CalaW <maker_cc@foxmail.com>
2023-08-29 17:22:44 -0400

    update a lot feature

--- link_to_texmf.sh
+++ link_to_texmf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 # This script is used to link classes in this directory to the user's texmf directory
 
+ln -s ~/Documents/Program/LaTeX-Template/CalaWArticle.cls ~/Library/texmf/tex/latex/calawcls/CalaWArticle.cls
 ln -s ~/Documents/Program/LaTeX-Template/CalaWCV.cls ~/Library/texmf/tex/latex/calawcls/CalaWCV.cls
 ln -s ~/Documents/Program/LaTeX-Template/CalaWHomework.cls ~/Library/texmf/tex/latex/calawcls/CalaWHomework.cls
 ln -s ~/Documents/Program/LaTeX-Template/CalaWMath.sty ~/Library/texmf/tex/latex/calawcls/CalaWMath.sty
